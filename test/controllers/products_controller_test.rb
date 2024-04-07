86ed906162a96cd31ced73a9f83dcf0e94085aff
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-04-07 14:20:12 -0400

    Create my first to test cases

---
+++ test/controllers/products_controller_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class ProductsControllerTest < ActionDispatch::IntegrationTest
+    test 'render a list of products' do
+
+    end
+
+    test 'render a detailed product page' do
+
+    end
+end
