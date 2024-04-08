dfaecc163d61100b826aef69c3255c1ef3de5250
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-04-07 20:34:07 -0400

    Create and run tests to checks the path of products

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -2,7 +2,9 @@ require 'test_helper'
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
     test 'render a list of products' do
+        get products_path
 
+        assert_response :success
     end
 
     test 'render a detailed product page' do
