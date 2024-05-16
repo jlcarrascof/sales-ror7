58b219a5e3927f200e8232cf07acc8ff1656c30d
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-05-16 18:58:14 -0400

    testing refactor: Update products controller to use partials for edit functionality

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -55,4 +55,16 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
         assert_response :success
         assert_select 'form'
     end
+
+    test 'allow to update a product' do
+        patch product_path(products(:ps4)), params: {
+            product: {
+                price: 165
+            }
+        }
+
+        assert_redirected_to products_path
+        assert_equal flash[:notice], 'Your product was updated successfully.'
+    end
+
 end
